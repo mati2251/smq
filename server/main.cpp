72c628adcddb58b4e966367f0fcdc1b4a3ec142f
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-11-29 14:16:46 +0100

    Improved c warning

--- server/main.cpp
+++ server/main.cpp
@@ -3,9 +3,6 @@
 int main()
 {
     std::cout << "Hello World" << std::endl;
-    float *ptr1, val=3.14;
-	char *ptr2;
-	ptr1 = &val;
-	ptr2 = &val;
+    float *ptr1, val=3.14; // te zmienne nie są używane
     return 0;
 }
