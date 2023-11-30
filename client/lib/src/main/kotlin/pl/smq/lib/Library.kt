5641da8813ebb1cb07e2c310c7c4afcb6ff4069e
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-11-30 15:43:07 +0100

    Added intentionally bad format to test ktlint

--- client/lib/src/main/kotlin/pl/smq/lib/Library.kt
+++ client/lib/src/main/kotlin/pl/smq/lib/Library.kt
@@ -1,8 +1,7 @@
 package pl.smq.lib
 
-class Library     {
-    fun someLibraryMethod():
-            Boolean {
-        return    true
+class  Library {
+    fun someLibraryMethod(): Boolean {
+        return true
     }
-}
+   }
