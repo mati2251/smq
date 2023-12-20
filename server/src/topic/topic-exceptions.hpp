782942b35f2416951356b50af1b7c508f32b06a8
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-12-20 09:06:36 +0100

    Fix  "Use of string after lifetime ends"

--- server/src/topic/topic-exceptions.hpp
+++ server/src/topic/topic-exceptions.hpp
@@ -8,7 +8,8 @@ public:
     TopicNotFoundException(const std::string &topic) : topic(topic) {}
     const char *what() const noexcept override
     {
-        return ("Topic " + topic + " not found").c_str();
+        std::string msg = "Topic " + topic + " not found";
+        return msg.c_str();
     }
 private:
     std::string topic;
@@ -20,7 +21,8 @@ public:
     ClientNotPublisherException(const int &client, const std::string &topic) : client(client), topic(topic) {}
     const char *what() const noexcept override
     {
-        return ("Client " + std::to_string(client) + " tried to send to not exist topic or client isn't register as publisher " + topic).c_str();
+        std::string  msg = "Client " + std::to_string(client) + " tried to send to not exist topic or client isn't register as publisher " + topic;
+        return msg.c_str();
     }
 private:
     int client;
@@ -33,7 +35,8 @@ public:
     ClientNotSubscriberException(const int &client, const std::string &topic) : client(client), topic(topic) {}
     const char *what() const noexcept override
     {
-        return ("Client " + std::to_string(client) + " tried to read from not exist topic or client isn't register as subscriber " + topic).c_str();
+        std::string msg = "Client " + std::to_string(client) + " tried to read from not exist topic or client isn't register as subscriber " + topic;
+        return msg.c_str();
     }
 private:
     int client;
@@ -46,7 +49,8 @@ public:
     ClientAlreadySubscriberException(const int &client, const std::string &topic) : client(client), topic(topic) {}
     const char *what() const noexcept override
     {
-        return ("Client " + std::to_string(client) + " already subscribed to " + topic).c_str();
+        std::string msg = "Client " + std::to_string(client) + " already subscribed to " + topic;
+        return msg.c_str();
     }
 private:
     int client;
@@ -59,7 +63,8 @@ public:
     ClientAlreadyPublisherException(const int &client, const std::string &topic) : client(client), topic(topic) {}
     const char *what() const noexcept override
     {
-        return ("Client " + std::to_string(client) + " already publish to " + topic).c_str();
+        std::string msg = "Client " + std::to_string(client) + " already publish to " + topic;
+        return msg.c_str();
     }
 private:
     int client;
