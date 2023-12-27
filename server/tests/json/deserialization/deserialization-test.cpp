b7a02fe92afc3ed1a50faa69ecb28a20dbfad505
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-12-27 17:59:37 +0100

    Fixed serialization tests

--- server/tests/json/deserialization/deserialization-test.cpp
+++ server/tests/json/deserialization/deserialization-test.cpp
@@ -1,51 +1,26 @@
 #include <catch2/catch_test_macros.hpp>
-#include "../../../src/json/deserialization/deserialization.h"
-#include "../../../src/structs/message.h"
+#include "../../../src/structs/request.h"
+#include "../../../src/request-util/deserialization/deserialization.h"
 
-TEST_CASE("message deserialization", "[deserialization]") {
-    message msg = deserializeMessage("{\"topic\"\n \f :\"topic\",\"content\":\"content \n\t\"}");
-    REQUIRE(msg.topic == "topic");
-    REQUIRE(msg.content == "content \n\t");
+TEST_CASE("request deserialization", "[deserialization]") {
+    std::string req_str = "MESSAGE\n1\ntopic\ncontent\n\n";
+    request req = deserializeRequest(req_str);
+    REQUIRE(req.type == request_type::MESSAGE);
+    REQUIRE(req.id == 1);
+    REQUIRE(req.topic == "topic");
+    REQUIRE(req.body == "content");
 }
 
-TEST_CASE("message deserialization with empty content", "[deserialization]") {
-    message msg = deserializeMessage("{\"topic\":\"topic\",\"content\":\"\"}");
-    REQUIRE(msg.topic == "topic");
-    REQUIRE(msg.content == "");
+TEST_CASE("deserialization with empty content", "[deserialization]") {
+    std::string req_str = "MESSAGE\n1\ntopic\n\n\n";
+    request req = deserializeRequest(req_str);
+    REQUIRE(req.type == request_type::MESSAGE);
+    REQUIRE(req.id == 1);
+    REQUIRE(req.topic == "topic");
+    REQUIRE(req.body == "");
 }
 
-TEST_CASE("message with bad json", "[deserialization]") {
-    REQUIRE_THROWS_AS(deserializeMessage("{\"topic\",:\"topic\",\"content\":\"content\""), DeserializationException);
-}
-
-TEST_CASE("message with bad key", "[deserialization]") {
-    REQUIRE_THROWS_AS(deserializeMessage("{\"topic2\":\"topic\",\"content\":\"content\""), DeserializationException);
-}
-
-TEST_CASE("message with too much parameters", "[deserialization]") {
-    REQUIRE_THROWS_AS(deserializeMessage("{\"topic\":\"topic\",\"content\":\"content\", \"content\": \"topic\"}"), DeserializationException);
-}
-
-TEST_CASE("action topic deserialization", "[deserialization]") {
-    action_topic action = deserializeAction("{\"topic\":\"topic\",\"action\":\"action\"}");
-    REQUIRE(action.topic == "topic");
-    REQUIRE(action.action == "action");
-}
-
-TEST_CASE("action topic deserialization with empty action", "[deserialization]") {
-    action_topic action = deserializeAction("{\"topic\":\"topic\",\"action\":\"\"}");
-    REQUIRE(action.topic == "topic");
-    REQUIRE(action.action == "");
-}
-
-TEST_CASE("action topic with bad json", "[deserialization]") {
-    REQUIRE_THROWS_AS(deserializeAction("{\"topic\",\"action\":\"action\"}"), DeserializationException);
-}
-
-TEST_CASE("action topic with bad key", "[deserialization]") {
-    REQUIRE_THROWS_AS(deserializeAction("{\"topic2\":\"topic\",\"action\":\"action\"}"), DeserializationException);
-}
-
-TEST_CASE("action topic with too much parameters", "[deserialization]") {
-    REQUIRE_THROWS_AS(deserializeAction("{\"topic\":\"topic\",\"action\":\"action\", \"action\": \"topic\"}"), DeserializationException);
+TEST_CASE("request deserialization exception", "[deserialization]") {
+    std::string req_str = "MESSAGE\n1\ntopic\ncontent";
+    REQUIRE_THROWS_AS(deserializeRequest(req_str), DeserializationException);
 }
--- server/tests/json/serialization/serialization-test.cpp
+++ server/tests/json/serialization/serialization-test.cpp
@@ -1,18 +1,18 @@
 
 #include <catch2/catch_test_macros.hpp>
-#include "../../../src/json/serialization/serialization.h"
-#include "../../../src/structs/message.h"
-
-TEST_CASE("message serialization", "[serialization]") {
-    message msg;
-    msg.topic = "topic";
-    msg.content = "content";
-    REQUIRE(serializeMessage(msg) == "MESSAGE\n{\"topic\": \"topic\",\"content\": \"content\"}");
+#include "../../../src/structs/request.h"
+#include "../../../src/request-util/serialization/serialization.h"
+TEST_CASE("request serialization", "[serialization]")
+{
+    request req = {request_type::MESSAGE, 1, "topic", "content"};
+    REQUIRE(serialize(req )== "MESSAGE\n1\ntopic\ncontent\n\n");
 }
 
-TEST_CASE("response serialization", "[serialization]") {
+TEST_CASE("response serialization", "[serialization]")
+{
     response res;
     res.code = response_code::SUCCESS;
+    res.request_id = 1;
     res.message = "message";
-    REQUIRE(serializeResponse(res) == "RESPONSE\n{\"code\": 0,\"message\": \"message\"}");
+    REQUIRE(serialize(res) == "RESPONSE\n1\n0\nmessage\n\n");
 }
