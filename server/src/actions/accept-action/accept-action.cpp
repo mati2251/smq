1cebd7ee617036046a6fa5f4ad1db6f439b957ba
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-12-27 10:28:35 +0100

    Accept action error handling

--- server/src/actions/accept-action/accept-action.cpp
+++ server/src/actions/accept-action/accept-action.cpp
@@ -8,6 +8,8 @@ void AcceptAction::action()
     if (client_fd == -1)
     {
         std::cerr << "Accept error: " << std::strerror(errno) << std::endl;
+        this->returnToEpoll();
+        return;
     }
     std::cout << "Accepted connection from " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port) << " as client " << client_fd << std::endl;
     ClientReadAction* client_read_action = new ClientReadAction(client_fd, this->efd);
@@ -16,12 +18,16 @@ void AcceptAction::action()
     {
         std::cerr << "Error epoll_ctl_add: " << std::strerror(errno) << std::endl;
     }
+    this->returnToEpoll();
+    ServerState::getInstance().addClient(new ClientWriteAction(client_fd, this->efd));
+}
+
+void AcceptAction::returnToEpoll(){
     int err = epoll_ctl(this->efd, EPOLL_CTL_MOD, this->fd, &this->ev);
     if (err == -1)
     {
         std::cerr << "Error epoll_ctl_mod: " << std::strerror(errno) << std::endl;
     }
-    ServerState::getInstance().addClient(new ClientWriteAction(client_fd, this->efd));
 };
 
 AcceptAction::AcceptAction(int fd, int efd) : EventAction(fd, efd)
