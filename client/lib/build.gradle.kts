e4b6b34713414c12ae53aa6b4c0f064095292d89
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2024-01-11 22:43:44 +0100

    Bump version

--- client/lib/build.gradle.kts
+++ client/lib/build.gradle.kts
@@ -35,7 +35,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "pl.smq"
             artifactId = "smq-lib"
-            version = "0.0.2"
+            version = "0.1.0"
 
             from(components["java"])
         }
