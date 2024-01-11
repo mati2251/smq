2bd215242b8ac738592d3af801ca1a2400d03bd4
McQbis <kubismaciej379@gmail.com>
2024-01-11 12:36:25 +0100

    negative value set no limit for package lifetime and buffer size

--- server/src/configure/configure.cpp
+++ server/src/configure/configure.cpp
@@ -1,13 +1,16 @@
 #include "configure.h"
 
-struct configure conf {.port = 3000, .package_lifetime = -1.0, .buffer_size = -1};
+struct configure conf {.port = 3000, 
+                        .package_lifetime = std::numeric_limits<float>::max(), 
+                        .buffer_size = std::numeric_limits<unsigned int>::max()};
 
 void set_port_conf(int port){
     conf.port = port;
 }
 
 void set_package_lifetime_conf(float package_lifetime){
-    conf.package_lifetime = package_lifetime;
+    if(package_lifetime >= 0)
+        conf.package_lifetime = package_lifetime;
 }
 
 void set_buffer_size_conf(int buffer_size){
@@ -18,10 +21,10 @@ float get_package_lifetime_conf(){
     return conf.package_lifetime;
 }
 
-float get_port_conf(){
+int get_port_conf(){
     return conf.port;
 }
 
-float get_buffer_size_conf(){
+unsigned int get_buffer_size_conf(){
     return conf.buffer_size;
 }
--- server/src/configure/configure.h
+++ server/src/configure/configure.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <iostream>
+#include <limits>
 
 struct configure{
     int port;
     float package_lifetime;
-    int buffer_size;
+    unsigned int buffer_size;
 };
 
 void set_port_conf(int port);
@@ -13,5 +14,5 @@ void set_package_lifetime_conf(float package_lifetime);
 void set_buffer_size_conf(int buffer_size);
 
 float get_package_lifetime_conf();
-float get_port_conf();
-float get_buffer_size_conf();
+int get_port_conf();
+unsigned int get_buffer_size_conf();
